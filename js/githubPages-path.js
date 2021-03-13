6ebf87d29d050da18a9f4cfc5fb97d6335bc4039
mm2411 <webmasa@outlook.com>
2021-03-13 12:17:12 +0900

    modified github pages path script.

--- js/githubPages-path.js
+++ js/githubPages-path.js
@@ -1,4 +1,10 @@
 $(function(){
+  $("head link[href^='/']").each(function() {
+    var obj = $(this);
+    var css__link = $("head link[href^='/']").attr("href");
+    obj.attr("href", "." + css__link);
+  });
+
   $("a[href^='/']").each(function() {
     var obj = $(this);
     var url_link = $("a[href^='/']").attr("href");
@@ -10,11 +16,4 @@ $(function(){
     var img_link = $("img").attr("src");
     $(obj).attr("src", "." + img_link);
   });
-  
-  $("head link[href^='/']").each(function() {
-    var obj = $(this);
-    var css__link = $("head link[href^='/']").attr("href");
-    obj.attr("href", "." + css__link);
-  });
-
 });
