05369b2d599ac18ad551a2f622d174dab3a79809
mm2411 <webmasa@outlook.com>
2021-03-19 10:49:08 +0900

    changed pon-design code to responsive.

--- pon-design/js/myjs.js
+++ pon-design/js/myjs.js
@@ -1,8 +1,17 @@
 
 $(function(){
-// 
-// SmoothScroll
-// 
+  // 
+  // slick
+  // 
+  $('.works__slider').slick({
+    autoplay: false,
+    arrows: true,
+    dots: true,
+  });
+
+  // 
+  // SmoothScroll
+  // 
   $('a[href^="#"]').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
@@ -12,23 +21,46 @@ $(function(){
     return false;
   });
 
-// 
-// Header Scroll
-// 
+  // 
+  // Header Scroll
+  // 
   //変更するcssクラス
   var nav = $('.hNavWrap');
-  
+  var ham = $('.hNav__hamburger');
+
   $(window).on('load scroll', function(){
     if ($(window).scrollTop() ) {
       nav.addClass('transform');
+      ham.addClass('transform-ham');
     } else {
       nav.removeClass('transform');
+      ham.removeClass('transform-ham');
     }
   });
+  // 
+  // Hamburger menu 
+  // 
+  var menu = $('.hNav__right');
+  var bar = $('.hNav__hamburger__bar');
+  var slick_arrow = $('.slick-arrow');
 
-// 
-// Scroll Button
-// 
+  ham.on('click', function() {
+    menu.toggleClass('is-active');
+    bar.toggleClass('is-cross');
+    slick_arrow.toggleClass('is-hidden');
+  });
+
+  var link = $('.hNav__rightLink');
+
+  link.on('click', function() {
+    menu.removeClass('is-active');
+    bar.removeClass('is-cross');
+    slick_arrow.removeClass('is-hidden');
+  });
+
+  // 
+  // Scroll Button
+  // 
   // var pageHeight = $('.TopView__heading').offset().top;
   var pageHeight = 300;
   var scrollBtn = $('.scroll__button');
@@ -40,15 +72,5 @@ $(function(){
       scrollBtn.addClass('hidden');
     }
   });
-
-// 
-// slick
-// 
-  $('.works__slider').slick({
-    autoplay: false,
-    arrows: true,
-    dots: true,
-  });
-
   
 });
